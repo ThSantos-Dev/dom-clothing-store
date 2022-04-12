46bb75bd40608f4e4b40d7fabb6c752cf4a9eb07
alunosenai <alunosenai@cfp127aluno.local>
2022-04-12 16:54:52 -0300

    script sql atualizado

--- cms/sql/script.sql
+++ cms/sql/script.sql
@@ -7,7 +7,7 @@ create table tbl_contatos (
     telefone varchar(18),
     email varchar(80) not null,
     obs text,
-    atualizaoes_email boolean
+    atualizacoes_email boolean
 );
 
 drop table tbl_contatos;
@@ -22,11 +22,25 @@ insert into tbl_contatos (
             atualizaoes_email
         ) 
         values (
-            "Yudi Playstation",
-            "(11)40028922",
-            "yudi.praysteition@gmail.com",
-            "Bora ganha um prayzinho???",
-            false
+            "Me ferrei",
+            "(11)k k kk k",
+            "me ferrei@gmail.com",
+            "hahahah ahahahah ahaha???",
+            0
         );
         
 select * from tbl_contatos;
+
+create table tbl_categorias(
+	id_categoria int not null auto_increment primary key,
+    nome varchar(80) not null
+);
+
+insert into tbl_categorias(nome)values('camisetas');
+
+select * from tbl_categorias;
+
+update tbl_categorias set nome= 'teste 2 editado' where id_categoria = 9
+
+
+	
